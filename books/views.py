1b1acc9b9cc7fe9cea10c6f75884393e1a58d2aa
murugancse <muruganaccetcse@gmail.com>
2021-10-18 08:55:38 +0530

    add books page and load data from json file

--- books/views.py
+++ books/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
+booksData = open('books.json').read()
+data = json.loads(booksData)
+
 def index(request):
-    context = { "name" : "Murugan"}
+    context = {'books': data}
     return render(request, 'books/index.html', context)
 
 def home_page(request):
